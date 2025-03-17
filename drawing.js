6d0469fdc974b188ef8f83f644d7fedaf0c68c8a
RJ993 <196080894+RJ993@users.noreply.github.com>
2025-03-17 15:57:31 -0400

    Implement minor code formatting

--- drawing.js
+++ drawing.js
@@ -1,9 +1,9 @@
-const container = document.querySelector(".container")
-const input = document.querySelector("input")
-const scale = document.querySelector(".scale")
+const container = document.querySelector(".container");
+const input = document.querySelector("input");
+const scale = document.querySelector(".scale");
 
 function reset(){
-    while (container.firstChild) {
+    while (container.firstChild){
         container.removeChild(container.firstChild);
     }
 }
@@ -13,20 +13,19 @@ function getRandomRGB(){
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r},${g},${b})`;
-    
 }
 
 function colorGrid(e){
-    e.target.style["background-color"] = getRandomRGB()
-    }
+    e.target.style["background-color"] = getRandomRGB();
+}
 
 function createGrid(){
     reset();
-    const scaleNumber = input.valueAsNumber
+    const scaleNumber = input.valueAsNumber;
     if(scaleNumber > 100 || !Number.isInteger(scaleNumber) || scaleNumber < 0){
-        input.focus()
+        input.focus();
     }else if (scaleNumber >= 1){
-        for (let i = 0; i < scaleNumber * scaleNumber; i++) {
+        for (let i = 0; i < scaleNumber * scaleNumber; i++){
         const gridUnit = document.createElement('div');
         gridUnit.classList.add('grid');
         container.appendChild(gridUnit);
@@ -34,8 +33,8 @@ function createGrid(){
         gridUnit.style.height = `${960 / scaleNumber}px`;
         }
     }
-    const grid = document.querySelectorAll(".grid")
-    grid.forEach(div => div.addEventListener('mouseenter', colorGrid))
+    const grid = document.querySelectorAll(".grid");
+    grid.forEach(div => div.addEventListener('mouseenter', colorGrid));
 }
 
-scale.addEventListener('click', createGrid) 
+scale.addEventListener('click', createGrid);
